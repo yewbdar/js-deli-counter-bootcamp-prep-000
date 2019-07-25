f7943365d89977da195fc8f077cc4e9fb6b8055d
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-25 17:05:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+ var katzDeli=[];
+function takeANumber(katzDeli,personName){
+ katzDeli.push(personName)
+  for(let i=0; i<katzDeli.length; i++){
+     if(katzDeli[i] == personName){
+     return `Welcome, ${katzDeli[i]}. You are number ${i + 1} in line.` 
+     }
+  }
+ }
+ 
+function nowServing(katzDeli){
+  if(katzDeli.length > 0){
+    return 'Currently serving' +' '+ katzDeli[0]+'.'
+    
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+  // return katzDeli.shift();
+}
